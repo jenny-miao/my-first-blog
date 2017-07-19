2a5cb6e90a5dd437f583c021b0945d05260ec513
jenny-miao <jenny.miao.2016@gmail.com>
2017-07-19 09:40:16 -0700

    reversing even further as the first step did not work

--- blog/forms.py
+++ blog/forms.py
@@ -7,9 +7,3 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
-
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ('author', 'text',)
